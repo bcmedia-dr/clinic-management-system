8eba81dea759c52595541e1f9d608e052b031283
nathan <bcm.drservice@gmail.com>
2026-03-17 09:56:31 +0800

    db.create_all() 移至 module 層級，Render 部署時自動建立新 table

--- app.py
+++ app.py
@@ -1011,7 +1011,8 @@ def _hm_json(h):
     }
 
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(host='0.0.0.0', port=8081, debug=True)
